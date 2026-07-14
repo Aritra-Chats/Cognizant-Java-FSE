7a1ed246f04bab5fdfa59edc543426672aaa9beb
Aritra-Chats <aritrachatterjee1904@gmail.com>
2026-07-14 16:31:06 +0530

    Completed Module 8 - Microservices with Spring Boot 3 and Spring Cloud hands-on
     - Completed Mandatory hands-on: Creating Microservices for account and loan
    
     - Completed Important hands-on: Create Eureka Discovery Server and register microservices

---
+++ Module 8 - Microservices with Spring Boot 3 and Spring Cloud/API-Gateway-Implementation/greet-service/src/main/java/org/cognizant/greetservice/controllers/GreetController.java
@@ -0,0 +1,12 @@
+package org.cognizant.greetservice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetController {
+    @GetMapping("/greet")
+    public String sayHello() {
+        return "Hello World!!";
+    }
+}
